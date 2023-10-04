66a7d7c7e98a6f448a2cb3417055f894024175b0
SebastianKurkowski1 <sebastiankurkowski1013@gmail.com>
2023-10-04 23:59:16 +0200

    request working

--- src/routes/homepage.tsx
+++ src/routes/homepage.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                await request(Method.GET, 'api/apod/2023-09-15');
+                await request(Method.POST, 'api/apod/2023-09-15');
             } catch (error) {
                 console.error(error);
             }
