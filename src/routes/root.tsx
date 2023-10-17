d3d4a76f17ea6f71f9cee3811d37a9611e405006
SebastianKurkowski1 <sebastiankurkowski1013@gmail.com>
2023-10-17 22:05:42 +0200

    removed unnecessary lines of code

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -7,17 +7,10 @@ import Particles from "react-particles";
 
 export default function Root() {
     const particlesInit = useCallback(async (engine: Engine) => {
-        console.log(engine);
-
-        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadFull(engine);
         await loadSlim(engine);
     }, []);
 
     const particlesLoaded = useCallback(async (container: Container | undefined) => {
-        await console.log(container);
     }, []);
 
 
